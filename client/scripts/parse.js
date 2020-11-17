f8a38e5439b9ffc46398e21106d893105b928778
jasonschreib <jwschreiber1@gmail.com>
2020-11-16 20:24:28 -0800

    Complete displaying messages from parse server

--- client/scripts/parse.js
+++ client/scripts/parse.js
@@ -4,6 +4,25 @@ var Parse = {
 
   create: function(message, successCB, errorCB = null) {
     // todo: save a message to the server
+    $.ajax({
+      url: Parse.server,
+      type: 'POST',
+      contentType: "application/json",
+      data: JSON.stringify(message),
+      success: successCB,
+      error: errorCB
+
+
+    });
+
+    //we'll be doing a POST in here and adding it to the .ajax command
+
+
+    //have to use ajax once at some point
+
+
+
+
   },
 
   readAll: function(successCB, errorCB = null) {
