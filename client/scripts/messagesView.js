0648cc27df4262d31e5f9e4d3e713b7e72b3ee17
jasonschreib <jwschreiber1@gmail.com>
2020-11-17 20:50:17 -0800

    Completed adding messages to DOM

--- client/scripts/messagesView.js
+++ client/scripts/messagesView.js
@@ -6,6 +6,31 @@ var MessagesView = {
   },
 
   render: function() {
+
+  },
+
+  // var message = {
+  //   username: 'Mel Brooks',
+  //   text: 'It\'s good to be the king',
+  //   roomname: 'lobby'
+  // };
+
+  renderMessage: function(message) {
+    //message is an object that contains a username, text, and roomname
+
+    //declare a render variable - render is equal to a template we create
+    var render = _.template(`
+      <div class="chat">
+        <div class="username">"tim"</div>
+        <div><%- message.text %></div>
+      </div>
+    `);
+    console.log(render);
+    //this template will include a div for username, text, and roomName
+
+
+    // //need to take render and append it to the div with id "chat"
+    $('#chats').append(render);
   }
 
 };
